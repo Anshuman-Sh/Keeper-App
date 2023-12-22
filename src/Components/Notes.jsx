2815c3eff69de9926460747ad43f1d5bf6892f1f
Anshuman Sharma <anshuman17sharma09@gmail.com>
2023-12-22 12:24:57 +0530

    Updates

--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -7,10 +7,10 @@ function Notes(props) {
             <div className='note'>
                 <h3>{props.title}</h3>
                 <p>{props.note}</p>
+            </div>
                 <button id="delete" onClick={()=>{
                   props.ondelete(props.id);
                 }}>Delete</button>
-            </div>
         </div>
     </>
   )
